468f10352a5e8237088927fc106476e055710fe7
Leon <132808925+Leon8326@users.noreply.github.com>
2025-05-22 19:35:58 +0200

    Update remote.sh

--- remote.sh
+++ remote.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export REMOTE=https://github.com/NarpIndex
+REMOTE="https://github.com/NarpIndex"
 export REMOTE_NAME="Main Remote"
 export REMOTE_ICON="🍫"
 export REMOTE_DESCRIPTION="Main Bitey Remote for Official Packages."
